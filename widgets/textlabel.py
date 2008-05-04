6837d348798c4955510b7dc839d82124ad43eb4c
Jeremy Sanders <jeremy@jeremysanders.net>
2008-05-04 16:21:11 +0000

    Fix id in file

--- widgets/textlabel.py
+++ widgets/textlabel.py
@@ -16,7 +16,7 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ###############################################################################
 
-# $Id: $
+# $Id$
 
 """For plotting one or more text labels on a graph."""
 
