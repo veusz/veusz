a8dd6e6939f72d05fcdfe8582da0bbe5e60c1920
Jeremy Sanders <jeremy@jeremysanders.net>
2010-02-27 18:03:13 +0000

    Fix multiple imports in custom imports. Fix order in import permissions dialog. Add document.log function to log error messages.

--- document/doc.py
+++ document/doc.py
@@ -49,6 +49,11 @@ import svg_export
 
 # python identifier
 identifier_re = re.compile(r'^[A-Za-z_][A-Za-z0-9_]*$')
+# for splitting
+identifier_split_re = re.compile(r'[A-Za-z_][A-Za-z0-9_]*')
+
+# python module
+module_re = re.compile(r'^[A-Za-z_\.]+$')
 
 # function(arg1, arg2...) for custom functions
 # not quite correct as doesn't check for commas in correct places
@@ -114,6 +119,10 @@ class Document( qt4.QObject ):
         self.suspendupdates = False
         if self.suspendchangeset != self.changeset:
             self.setModified()
+
+    def log(self, message):
+        """Log a message - this is emitted as a signal."""
+        self.emit( qt4.SIGNAL("sigLog"), message )
 
     def applyOperation(self, operation):
         """Apply operation to the document.
@@ -677,6 +686,10 @@ class Document( qt4.QObject ):
     def _processSafeImports(self, module, symbols):
         """Check what symbols are safe to import."""
 
+        # empty list
+        if not symbols:
+            return symbols
+
         # do import anyway
         if setting.transient_settings['unsafe_mode']:
             return symbols
@@ -723,6 +736,60 @@ class Document( qt4.QObject ):
 
         return toimport
 
+    def _updateEvalContextImport(self, module, val):
+        """Add an import statement to the eval function context."""
+        if module_re.match(module):
+            # work out what is safe to import
+            symbols = identifier_split_re.findall(val)
+            toimport = self._processSafeImports(module, symbols)
+            if toimport:
+                defn = 'from %s import %s' % (module,
+                                              ', '.join(toimport))
+                try:
+                    exec defn in self.eval_context
+                except Exception:
+                    self.log("Failed to import '%s' from "
+                             "module '%s'" % (', '.join(toimport),
+                                              module))
+                    return
+
+            delta = set(symbols)-set(toimport)
+            if delta:
+                self.log("Did not import '%s' from module '%s'" %
+                         (', '.join(list(delta)), module))
+
+        else:
+            self.log( "Invalid module name '%s'" % module )
+
+    def _updateEvalContextFuncOrConst(self, ctype, name, val):
+        """Update a function or constant in eval function context."""
+
+        if ctype == 'constant':
+            if not identifier_re.match(name):
+                self.log( "Invalid constant name '%s'" % name )
+                return
+            defn = val
+        elif ctype == 'function':
+            m = function_re.match(name)
+            if not m:
+                self.log( "Invalid function specification '%s'" % name )
+                return
+            name = funcname = m.group(1)
+            args = m.group(2)
+            defn = 'lambda %s: %s' % (args, val)
+
+        # evaluate, but we ignore any unsafe commands or exceptions
+        checked = utils.checkCode(defn)
+        if checked is not None:
+            self.log( "Expression '%s' failed safe code test" %
+                      defn )
+            return
+        try:
+            self.eval_context[name] = eval(defn, self.eval_context)
+        except Exception, e:
+            self.log( "Error evaluating '%s': '%s'" %
+                      (name, unicode(e)) )
+
     def updateEvalContext(self):
         """To be called after custom constants or functions are changed.
         This sets up a safe environment where things can be evaluated
@@ -747,42 +814,10 @@ class Document( qt4.QObject ):
         for ctype, name, val in self.customs:
             name = name.strip()
             val = val.strip()
-
-            if ctype in ('constant', 'function'):
-                if ctype == 'constant':
-                    if not identifier_re.match(name):
-                        continue
-                    defn = val
-                elif ctype == 'function':
-                    m = function_re.match(name)
-                    if not m:
-                        continue
-                    name = funcname = m.group(1)
-                    args = m.group(2)
-                    defn = 'lambda %s: %s' % (args, val)
-
-                # evaluate, but we ignore any unsafe commands or exceptions
-                checked = utils.checkCode(defn)
-                if checked is not None:
-                    continue
-                try:
-                    c[name] = eval(defn, c)
-                except:
-                    pass
-
+            if ctype == 'constant' or ctype == 'function':
+                self._updateEvalContextFuncOrConst(ctype, name, val)
             elif ctype == 'import':
-                module = name
-                symbols = identifier_re.findall(val)
-                if identifier_re.match(module) and symbols:
-                    # work out what is safe to import
-                    symbols = self._processSafeImports(module, symbols)
-                    if symbols:
-                        defn = 'from %s import %s' % (module,
-                                                      ', '.join(symbols))
-                        try:
-                            exec defn in c
-                        except:
-                            pass
+                self._updateEvalContextImport(name, val)
             else:
                 raise ValueError, 'Invalid custom type'
 
