12aa4e9f216b8347ca99b9dea6be1bcd82ab7a70
Jeremy Sanders <jeremy@jeremysanders.net>
2010-06-05 17:25:34 +0000

    Cleanups to clipping module

--- helpers/src/polygonclip.cpp
+++ helpers/src/polygonclip.cpp
@@ -1,44 +1,35 @@
-/*
-    Copyright (C) 2010 Jeremy Sanders
+// Copyright (C) 2010 Jeremy Sanders
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
- This code uses the Sutherland Hodgman algorithm to clip a polygon
- It is based on algorithm of a C++ version by Sjaak Priester
- see http://www.codeguru.com/Cpp/misc/misc/graphics/article.php/c8965/
-*/
+// This code uses the Sutherland Hodgman algorithm to clip a polygon
+// It is inspired by another version by Sjaak Priester
+// see http://www.codeguru.com/Cpp/misc/misc/graphics/article.php/c8965/
 
 #include "polygonclip.h"
 
-/* makes calculations of intercept of line and edge easier */
-/* vara = x and varb = y if calculating a x value, and vice versa */
-#define INTERCEPT(pt, lastpt, edgeval, vara, varb)	\
-  (edgeval - pt.vara()) * (lastpt.varb() - pt.varb()) /	\
-  (lastpt.vara() - pt.vara()) + pt.varb()
-
-/* macro to clip point against edge
-   - edge: name of edge for clipping
-   - isinside: f(pt) to return whether point is inside edge
-   - interceptx: value of x for new point when line intercepts edge
-   - intercepty: value of y for new point when line intercepts edge
-   - next: function to call next to clip point
-*/
-#define CLIPEDGE(edge, isinside, interceptx, intercepty, next)	\
+// macro to clip point against edge
+//   - edge: name of edge for clipping
+//   - isinside: test whether point is inside edge
+//   - intercept: function to calculate coordinate where line
+//     crosses edge
+//   - next: function to call next to clip point
+#define CLIPEDGE(edge, isinside, intercept, next)		\
   void edge##ClipPoint(const QPointF& pt)			\
   {								\
+    QPointF& lastpt = edge##last;				\
     if( edge##is1st )						\
       {								\
 	/* do nothing */					\
@@ -49,35 +40,38 @@
       {								\
 	if( isinside(pt) )					\
 	  {							\
-	    if( ! isinside(edge##last) )			\
-	      {							\
-		/* this point inside and last point outside */	\
-		next(QPointF(interceptx, intercepty));		\
-	      }							\
+	    if( ! isinside(lastpt) )				\
+	      /* this point inside and last point outside */	\
+	      next( intercept(clip.edge(), pt, lastpt) );	\
 	    next(pt);						\
 	  }							\
 	else							\
 	  {							\
-	    if( isinside(edge##last) )				\
-	      {							\
-		/* this point outside and last point inside */	\
-		next(QPointF(interceptx, intercepty));		\
-	      }							\
+	    if( isinside(lastpt) )				\
+	      /* this point outside and last point inside */	\
+	      next( intercept(clip.edge(), pt, lastpt) );	\
 	    /* else do nothing if both outside */		\
 	  }							\
       }								\
 								\
-    edge##last = pt;						\
+    lastpt = pt;						\
   }
-
-// tests to see whether point is inside particular side
-#define INSIDEBOTTOM(pt) (pt.y() <= clip.bottom())
-#define INSIDETOP(pt) (pt.y() >= clip.top())
-#define INSIDERIGHT(pt) (pt.x() <= clip.right())
-#define INSIDELEFT(pt) (pt.x() >= clip.left())
 
 namespace
 {
+  inline QPointF interceptVert(qreal horzval,
+			       const QPointF& pt1, const QPointF& pt2)
+  {
+    const qreal gradient = (pt2.y()-pt1.y()) / (pt2.x()-pt1.x());
+    return QPointF(horzval, (horzval - pt1.x())*gradient + pt1.y());
+  }
+  inline QPointF interceptHorz(qreal vertval,
+			       const QPointF& pt1, const QPointF& pt2)
+  {
+    const qreal gradient = (pt2.x()-pt1.x()) / (pt2.y()-pt1.y());
+    return QPointF((vertval - pt1.y())*gradient + pt1.x(), vertval);
+  }
+
   struct State
   {
     State(const QRectF& rect, QPolygonF& out)
@@ -86,23 +80,29 @@ namespace
     {
     }
 
+    // tests for whether point is inside of outside of each side
+    inline bool insideBottom(const QPointF& pt) const
+    {
+      return pt.y() <= clip.bottom();
+    }
+    inline bool insideTop(const QPointF& pt) const
+    {
+      return pt.y() >= clip.top();
+    }
+    inline bool insideRight(const QPointF& pt) const
+    {
+      return pt.x() <= clip.right();
+    }
+    inline bool insideLeft(const QPointF& pt) const
+    {
+      return pt.x() >= clip.left();
+    }
+
     // add functions for clipping to each edge
-    CLIPEDGE(bottom, INSIDEBOTTOM,
-	     INTERCEPT(pt, bottomlast, clip.bottom(), y, x),
-	     clip.bottom(),
-	     writeClipPoint)
-    CLIPEDGE(top, INSIDETOP,
-	     INTERCEPT(pt, toplast, clip.top(), y, x),
-	     clip.top(),
-	     bottomClipPoint)
-    CLIPEDGE(right, INSIDERIGHT,
-	     clip.right(),
-	     INTERCEPT(pt, rightlast, clip.right(), x, y),
-	     topClipPoint)
-    CLIPEDGE(left, INSIDELEFT,
-	     clip.left(),
-	     INTERCEPT(pt, leftlast, clip.left(), x, y),
-	     rightClipPoint)
+    CLIPEDGE(bottom, insideBottom, interceptHorz, writeClipPoint)
+    CLIPEDGE(top, insideTop, interceptHorz, bottomClipPoint)
+    CLIPEDGE(right, insideRight, interceptVert, topClipPoint)
+    CLIPEDGE(left, insideLeft, interceptVert, rightClipPoint)
 
     // finally writes to output
     void writeClipPoint(const QPointF& pt)
@@ -155,7 +155,7 @@ void polygonClip(const QPolygonF& inpoly,
 //      << QPointF(200, 200) << QPointF(100, 200);
 //   QPolygonF out;
  
-//   polygonClip(in, QRectF(QPointF(0,0),QPointF(150,150)), out);
+//   polygonClip(in, QRectF(QPointF(0,0),QPointF(1000,1000)), out);
 
 //   for(QPolygonF::const_iterator i = out.begin(); i != out.end(); ++i)
 //     printf("%g %g\n", i->x(), i->y());
--- helpers/src/polygonclip.h
+++ helpers/src/polygonclip.h
@@ -1,17 +1,18 @@
-    // Copyright (C) 2010 Jeremy Sanders
-
-    // This program is free software: you can redistribute it and/or modify
-    // it under the terms of the GNU General Public License as published by
-    // the Free Software Foundation, either version 3 of the License, or
-    // (at your option) any later version.
-
-    // This program is distributed in the hope that it will be useful,
-    // but WITHOUT ANY WARRANTY; without even the implied warranty of
-    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    // GNU General Public License for more details.
-
-    // You should have received a copy of the GNU General Public License
-    // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright (C) 2010 Jeremy Sanders
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////
 
 #ifndef POLYGONCLIP_HH
 #define POLYGONCLIP_HH
@@ -20,8 +21,9 @@
 #include <QPolygonF>
 #include <QRectF>
 
+// clip input polygon to clipping rectangle, filling outpoly
 void polygonClip(const QPolygonF& inpoly,
 		 const QRectF& cliprect,
-		 QPolygonF& out);
+		 QPolygonF& outpoly);
 
 #endif
