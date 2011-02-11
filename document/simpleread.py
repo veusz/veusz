d212be6ffa0bfdb2fcd5611a1a841ab775282c4f
Jeremy Sanders <jeremy@jeremysanders.net>
2011-02-11 17:20:13 +0000

    Fix windows \r characters breaking data import

--- document/simpleread.py
+++ document/simpleread.py
@@ -347,15 +347,15 @@ class Stream(object):
     # this regular expression is for splitting up the stream into words
     # I'll try to explain this bit-by-bit (these are ORd, and matched in order)
     split_re = re.compile( r'''
-    `.+?`[^ \t\n#!%;]* | # match dataset name quoted in back-ticks
-                         # we also need to match following characters to catch
-                         # corner cases in the descriptor
+    `.+?`[^ \t\n\r#!%;]* | # match dataset name quoted in back-ticks
+                           # we also need to match following characters to catch
+                           # corner cases in the descriptor
     u?"" |          # match empty double-quoted string
     u?".*?[^\\]" |  # match double-quoted string, ignoring escaped quotes
     u?'' |          # match empty single-quoted string
     u?'.*?[^\\]' |  # match single-quoted string, ignoring escaped quotes
     [#!%;].* |      # match comment to end of line
-    [^ \t\n#!%;]+   # match normal space/tab separated items
+    [^ \t\n\r#!%;]+ # match normal space/tab separated items
     ''', re.VERBOSE )
 
     def __init__(self):
