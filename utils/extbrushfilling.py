8fb0f3c894dcf5a1eab2f02b771cb209d0a88677
Jeremy Sanders <jeremy@jeremysanders.net>
2012-02-11 17:35:51 +0000

    Make hatching more sophisticated. Properly calculate repeat interval. Add more styles.

--- utils/extbrushfilling.py
+++ utils/extbrushfilling.py
@@ -1,72 +1,164 @@
-"""Paint hatched fills."""
+#    Copyright (C) 2012 Jeremy S. Sanders
+#    Email: Jeremy Sanders <jeremy@jeremysanders.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+###############################################################################
+
+"""Paint fills with extended brush class.
+
+Paints solid, hatching and various qt brushes
+"""
 
 import numpy as N
 import veusz.qtall as qt4
+import math
 
 try:
     from veusz.helpers.qtloops import plotLinesToPainter, polygonClip
 except ImportError:
     from slowfuncs import plotLinesToPainter, polygonClip
 
-def hatcher(painter, pen, painterpath, deltax, deltay):
+def _hatcher(painter, pen, painterpath, spacing, hatchlist):
     """Draw hatching on painter path given."""
 
     painter.save()
     painter.setPen(pen)
-
     painter.setClipPath(painterpath)
 
     # this is the bounding box of the path
     bb = painter.clipPath().boundingRect()
 
-    # compute max number of steps
-    numsteps = 0
-    if deltax != 0:
-        numsteps += bb.width() / abs(deltax) + 1
-    if deltay != 0:
-        numsteps += bb.height() / abs(deltay) + 1
-    if numsteps == 0:
-        raise ValueError, "deltax or deltay must be non-zero"
+    for params in hatchlist:
+        # scale values
+        offsetx, offsety, deltax, deltay = [x*spacing for x in params]
 
-    # initial point inside the box
-    # we want to make sure shifted regions lines in the same places
-    if deltax == 0:
-        startx = bb.left()
-    else:
-        startx = ( int(bb.left() / deltax) + 1 )*deltax
-    if deltay == 0:
-        starty = bb.bottom()
-    else:
-        starty = ( int(bb.bottom() / deltay) + 1 )*deltay
+        # compute max number of steps
+        numsteps = 0
+        if deltax != 0:
+            numsteps += int(bb.width() / abs(deltax)) + 4
+        if deltay != 0:
+            numsteps += int(bb.height() / abs(deltay)) + 4
 
-    # normalise line vector
-    mag = N.sqrt( deltax**2 + deltay**2 )
-    linedx, linedy = -deltay/mag, deltax/mag
-    scale = max( bb.width(), bb.height() ) * 4
+        # calculate repeat position in x and y
+        # we start the positions of lines at multiples of these distances
+        # to ensure the pattern doesn't shift if the area does
 
-    # construct points along lines
-    # this is scales to ensure the lines are bigger than the box
-    idx = N.arange(-numsteps, numsteps)
-    x = idx*deltax + startx
-    y = idx*deltay + starty
-    x1 = x - scale*linedx
-    x2 = x + scale*linedx
-    y1 = y - scale*linedy
-    y2 = y + scale*linedy
+        # distances between lines
+        mag = math.sqrt(deltax**2 + deltay**2)
+        # angle to x of perpendicular lines
+        theta = math.atan2(deltay, deltax)
+        s, c = math.sin(theta), math.cos(theta)
+        intervalx = intervaly = 1.
+        if abs(c) > 1e-4: intervalx = abs(mag / c)
+        if abs(s) > 1e-4: intervaly = abs(mag / s)
 
-    # plot lines, bounding to bb
-    plotLinesToPainter(painter, x1, y1, x2, y2, bb)
+        startx = int(bb.left() / intervalx) * intervalx + offsetx
+        starty = int(bb.top()  / intervaly) * intervaly + offsety
+
+        # normalise line vector
+        linedx, linedy = -deltay/mag, deltax/mag
+        # scale of lines from start position
+        scale = max( bb.width(), bb.height() ) * 4
+
+        # construct points along lines
+        # this is scales to ensure the lines are bigger than the box
+        idx = N.arange(-numsteps, numsteps)
+        x = idx*deltax + startx
+        y = idx*deltay + starty
+        x1 = x - scale*linedx
+        x2 = x + scale*linedx
+        y1 = y - scale*linedy
+        y2 = y + scale*linedy
+
+        # plot lines, clipping to bb
+        plotLinesToPainter(painter, x1, y1, x2, y2, bb)
 
     painter.restore()
 
+# list of fill styles
+extfillstyles = (
+    'solid', 'horizontal', 'vertical', 'cross',
+    'forward diagonals', 'backward diagonals',
+    'diagonal cross',
+    'forward 2', 'backward 2', 'forward 3',
+    'backward 3', 'forward 4', 'backward 4',
+    'forward 5', 'backward 5',
+    'diagonal cross 2', 'diagonal cross 3',
+    'diagonal cross 4', 'diagonal cross 5',
+    'vertical forward', 'vertical backward',
+    'horizontal forward', 'horizontal backward',
+    'star',
+    'triangles 1', 'triangles 2', 'triangles 3', 'triangles 4',
+    'horizontal double', 'vertical double',
+    'forward double', 'backward double',
+    'double cross', 'double diagonal cross',
+    '94% dense', '88% dense', '63% dense', '50% dense',
+    '37% dense', '12% dense', '6% dense',
+    )
+
+# hatching styles
+# map names to lists of (offsetx, offsety, gradx, grady)
 _hatchmap = {
-    'horizontal': ((0., 1.), ),
-    'vertical': ((1., 0.), ),
-    'cross': ( (0., 1.), (1., 0.), ),
-    'forward diagonals': ( (0.7071, -0.7071), ),
-    'backward diagonals': ( (0.7071, 0.7071), ),
-    'diagonal cross': ( (0.7071, 0.7071), (0.7071, -0.7071), ),
-}
+    'horizontal': ((0, 0, 0, 1), ),
+    'vertical': ((0, 0, 1, 0), ),
+    'cross': ( (0, 0, 0, 1), (0, 0, 1, 0), ),
+    'forward diagonals': ( (0, 0, 0.7071, -0.7071), ),
+    'backward diagonals': ( (0, 0, 0.7071, 0.7071), ),
+    'diagonal cross': ( (0, 0, 0.7071, 0.7071), (0, 0, 0.7071, -0.7071), ),
+    'forward 2': ( (0, 0, 0.8660, -0.5), ),
+    'backward 2': ( (0, 0, 0.8660, 0.5), ),
+    'forward 3': ( (0, 0, 0.5, -0.8660), ),
+    'backward 3': ( (0, 0, 0.5, 0.8660), ),
+    'forward 4': ( (0, 0, 0.2588, -0.9659), ),
+    'backward 4': ( (0, 0, 0.2588, 0.9659), ),
+    'forward 5': ( (0, 0, 0.9659, -0.2588), ),
+    'backward 5': ( (0, 0, 0.9659, 0.2588), ),
+    'diagonal cross 2': ( (0, 0, 0.8660, -0.5), (0, 0, 0.8660, 0.5), ),
+    'diagonal cross 3': ( (0, 0, 0.5, -0.8660), (0, 0, 0.5, 0.8660), ),
+    'diagonal cross 4': ( (0, 0, 0.9659, -0.2588), (0, 0, 0.9659, 0.2588), ),
+    'diagonal cross 5': ( (0, 0, 0.2588, 0.9659), (0, 0, 0.2588, -0.9659), ),
+    'vertical forward': ( (0, 0, 1, 0), (0, 0, 0.7071, -0.7071), ),
+    'vertical backward': ( (0, 0, 1, 0), (0, 0, 0.7071, 0.7071), ),
+    'horizontal forward': ( (0, 0, 0, 1), (0, 0, 0.7071, -0.7071), ),
+    'horizontal backward': ( (0, 0, 0, 1), (0, 0, 0.7071, 0.7071), ),
+    'star': ( (0, 0, 2, 0), (0, 0, 0, 2), (0, 0, -1, 1), (0, 0, 1, 1), ),
+    'triangles 1': ( (0, 0, 2, 0), (0, 0, 0, 2), (0, 0, -1, 1),),
+    'triangles 2': ( (0, 0, 2, 0), (0, 0, 0, 2), (0, 0, 1, 1),),
+    'triangles 3': ( (0, 0, 0, 1), (0, 0, -1, 1), (0, 0, 1, 1), ),
+    'triangles 4': ( (0, 0, 1, 0), (0, 0, -1, 1), (0, 0, 1, 1), ),
+    'horizontal double': ((0, 0, 0, 1), (0, 0.2828, 0, 1), ),
+    'vertical double': ((0, 0, 1, 0), (0.2828, 0, 1, 0), ),
+    'forward double': ((0, 0, 0.7071, -0.7071), (0.4,0, 0.7071, -0.7071)),
+    'backward double': ((0, 0, 0.7071, 0.7071), (0.4,0, 0.7071, 0.7071)),
+    'double cross': ((0, 0, 0, 1), (0, 0.2828, 0, 1),
+                     (0, 0, 1, 0), (0.2828, 0, 1, 0),),
+    'double diagonal cross': ((0, 0, 0.7071, -0.7071), (0.4,0, 0.7071, -0.7071),
+                              (0, 0, 0.7071, 0.7071), (0.4,0, 0.7071, 0.7071)),
+    }
+
+# convert qt-specific fill styles into qt styles
+_fillcnvt = {
+    'solid': qt4.Qt.SolidPattern,
+    '94% dense': qt4.Qt.Dense1Pattern,
+    '88% dense': qt4.Qt.Dense2Pattern,
+    '63% dense': qt4.Qt.Dense3Pattern,
+    '50% dense': qt4.Qt.Dense4Pattern,
+    '37% dense': qt4.Qt.Dense5Pattern,
+    '12% dense': qt4.Qt.Dense6Pattern,
+    '6% dense': qt4.Qt.Dense7Pattern
+    }
 
 def brushExtFillPath(painter, extbrush, path):
     """Use an BrushExtended settings object to fill a path on painter."""
@@ -74,7 +166,15 @@ def brushExtFillPath(painter, extbrush, path):
     if extbrush.hide:
         return
     
-    if extbrush.style in _hatchmap:
+    style = extbrush.style
+    if style in _fillcnvt:
+        # standard fill: use Qt styles for painting
+        color = qt4.QColor(extbrush.color)
+        color.setAlphaF( (100-extbrush.transparency) / 100.)
+        brush = qt4.QBrush( color, _fillcnvt[style] )
+        painter.fillPath(path, brush)
+
+    elif style in _hatchmap:
         # fill with hatching
 
         if not extbrush.backhide:
@@ -87,25 +187,17 @@ def brushExtFillPath(painter, extbrush, path):
         color = qt4.QColor(extbrush.color)
         color.setAlphaF( (100-extbrush.transparency) / 100.)
         width = extbrush.get('linewidth').convert(painter)
-        style, dashpattern = extbrush.get('linestyle')._linecnvt[
+        lstyle, dashpattern = extbrush.get('linestyle')._linecnvt[
             extbrush.linestyle]
-        pen = qt4.QPen(color, width, style)
+        pen = qt4.QPen(color, width, lstyle)
 
         if dashpattern:
             pen.setDashPattern(dashpattern)
 
         # do hatching with spacing
         spacing = extbrush.get('patternspacing').convert(painter)
-
-        # iterate over each hatch
-        for dx, dy in _hatchmap[extbrush.style]:
-            hatcher(painter, pen, path, dx*spacing, dy*spacing)
-    else:
-        # standard fill: use Qt styles for painting
-        color = qt4.QColor(extbrush.color)
-        color.setAlphaF( (100-extbrush.transparency) / 100.)
-        brush = qt4.QBrush( color, extbrush.get('style').qtStyle() )
-        painter.fillPath(path, brush)
+        if spacing > 0:
+            _hatcher(painter, pen, path, spacing, _hatchmap[style])
 
 def brushExtFillPolygon(painter, extbrush, cliprect, polygon):
     """Fill a polygon with an extended brush."""
