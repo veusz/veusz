c437e59512aa34d4c3b9549b936a79fa9218ac46
Jeremy Sanders <jeremy@jeremysanders.net>
2012-10-07 13:39:48 +0200

    Fix bug report Fri, 05 Oct 2012 14:58:12 +0000
    Veusz version: 1.16
    Python version: 2.7.3 (default, Apr 24 2012, 00:00:54)
    [GCC 4.7.0 20120414 (prerelease)]
    Python platform: linux2
    Numpy version: 1.6.2
    Qt version: 4.8.3
    PyQt version: 4.9.4
    SIP version: 4.13.3
    Date: Fri, 05 Oct 2012 14:58:12 +0000
    
    Traceback (most recent call last):
      File "/usr/lib/python2.7/site-packages/veusz/dialogs/plugin.py", line 174, in runPlugin
        results = doc.applyOperation(op)
      File "/usr/lib/python2.7/site-packages/veusz/document/doc.py", line 177, in applyOperation
        retn = operation.do(self)
      File "/usr/lib/python2.7/site-packages/veusz/document/operations.py", line 1466, in do
        self.plugin.apply(ifc, self.fields)
      File "/usr/lib/python2.7/site-packages/veusz/plugins/toolsplugin.py", line 178, in apply
        col = colatidx(idx)
      File "/usr/lib/python2.7/site-packages/veusz/plugins/toolsplugin.py", line 169, in colatidx
        H = i * (H2-H1) / float(numwidgets-1) + H1
    ZeroDivisionError: float division by zero

--- plugins/toolsplugin.py
+++ plugins/toolsplugin.py
@@ -166,9 +166,11 @@ class ColorsSequence(ToolsPlugin):
 
         def colatidx(i):
             """Get color in range 0...numwidgets-1."""
-            H = i * (H2-H1) / float(numwidgets-1) + H1
-            S = i * (S2-S1) / float(numwidgets-1) + S1
-            V = i * (V2-V1) / float(numwidgets-1) + V1
+            div = float(max(numwidgets-1, 1))
+
+            H = i * (H2-H1) / div + H1
+            S = i * (S2-S1) / div + S1
+            V = i * (V2-V1) / div + V1
             return str(qt4.QColor.fromHsv(H, S, V).name())
 
         idx = 0
