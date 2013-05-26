6cc81775c28e4c084fee3d4835ff34146c608427
Jeremy Sanders <jeremy@jeremysanders.net>
2013-05-26 16:37:53 +0200

    Don't use multimodel when empty

--- dialogs/dataeditdialog.py
+++ dialogs/dataeditdialog.py
@@ -477,7 +477,7 @@ class DataEditDialog(VeuszDialog):
                 model = DatasetTableModel1D(self, self.document, names[0])
             elif ds.dimensions == 2:
                 model = DatasetTableModel2D(self, self.document, names[0])
-        else:
+        elif len(names) > 1:
             model = DatasetTableModelMulti(self, self.document, names)
 
         # disable context menu if no menu
