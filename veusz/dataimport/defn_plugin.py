ecebdfc6b663376a089e07f3b2e1d12ebef4e571
Jeremy Sanders <jeremy@jeremysanders.net>
2014-04-04 20:37:39 +0200

    Fix ignored xcent/ycent for Dataset2D import plugins (Fix github #60)

--- veusz/dataimport/defn_plugin.py
+++ veusz/dataimport/defn_plugin.py
@@ -111,7 +111,8 @@ class OperationDataImportPlugin(base.OperationDataImportBase):
             elif isinstance(d, plugins.Dataset2D):
                 ds = document.Dataset2D(data=d.data,
                                         xrange=d.rangex, yrange=d.rangey,
-                                        xedge=d.xedge, yedge=d.yedge)
+                                        xedge=d.xedge, yedge=d.yedge,
+                                        xcent=d.xcent, ycent=d.ycent)
             elif isinstance(d, plugins.DatasetText):
                 ds = document.DatasetText(data=d.data)
             elif isinstance(d, plugins.DatasetDateTime):
