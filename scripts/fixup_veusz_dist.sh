54d91e02aedc3c13dfd001c4e300bb655fb6f894
Jeremy Sanders <jeremy@jeremysanders.net>
2014-07-21 20:40:54 +0200

    Update py2app scripts

--- scripts/fixup_veusz_dist.sh
+++ scripts/fixup_veusz_dist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-distdir=/Users/apple/veusz-git/veusz/dist/Veusz.app
-qtdir=/Developer/Applications/Qt
+distdir=/Users/jss/veusz/dist/Veusz.app
+qtdir=/usr/local/Trolltech/Qt-4.8.4
 
 cat > ${distdir}/Contents/Resources/qt.conf <<EOF
 [paths]
@@ -17,9 +17,19 @@ for plugin in iconengines imageformats; do
     for x in *.dylib; do
 	frames=`otool -L $x | grep Qt | awk '{print $1}'`
 	for f in $frames; do
-	    install_name_tool -change $f @executable_path/../Frameworks/$f $x
+	    # get part of path after frameworks
+	    relpath=$(echo $f | rev | cut -d/ -f1-4 | rev)
+	    install_name_tool -change $f @executable_path/../Frameworks/${relpath} $x
 	done
     done
     popd
 done
 
+find ${distdir} -name "*_debug*" -print0 | xargs -0 rm
+
+# delete unused frameworks
+for x in QtHelp.framework QtNetwork.framework QtOpenGL.framework QtSql.framework QtTest.framework QtXmlPatterns.framework QtDesigner.framework; do
+    rm -rf ${distdir}/Contents/Frameworks/${x}
+done
+
+rm ${distir}/Contents/Frameworks/libQtCLucene*
