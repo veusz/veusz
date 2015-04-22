94de815f5963dad79478f7b7ee55389892608344
Jeremy Sanders <jeremy@jeremysanders.net>
2015-04-22 19:35:42 +0200

    Fix compile warning

--- veusz/helpers/src/qtmml/qtmmlwidget.cpp
+++ veusz/helpers/src/qtmml/qtmmlwidget.cpp
@@ -3392,7 +3392,7 @@ MmlNode *MmlDocument::domToMml(const QDomNode &dom_node, bool *ok, QString *erro
 
     QDomNamedNodeMap dom_attr = dom_node.attributes();
     MmlAttributeMap mml_attr;
-    for (int i = 0; i < dom_attr.length(); ++i) {
+    for (int i = 0; i < int(dom_attr.length()); ++i) {
 	QDomNode attr_node = dom_attr.item(i);
 	Q_ASSERT(!attr_node.nodeName().isNull());
 	Q_ASSERT(!attr_node.nodeValue().isNull());
