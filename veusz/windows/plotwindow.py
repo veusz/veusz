c0eb52223c56095d8d01ff2299f72bf54cbcaf06
Emmanuel <KDB2@users.noreply.github.com>
2016-09-18 14:25:10 +0200

    fix shift + scroll part 2. Always use pixelDelta first
    Fallback to angleDelta if pixelDelta not available on user hardware.

--- veusz/windows/plotwindow.py
+++ veusz/windows/plotwindow.py
@@ -846,7 +846,10 @@ class PlotWindow( qt4.QGraphicsView ):
                 self.sumwheeldelta -= 120
         elif event.modifiers() & qt4.Qt.ShiftModifier:
             # scroll horizontally if shift is held down
-            d = event.angleDelta()
+            d = event.pixelDelta()
+            if d.x() == 0 and d.y() == 0:
+                # Fallback mode to angleDelta
+                d = event.angleDelta()
             delta = d.x() if d.x() != 0 else d.y()
             scrollx = self.horizontalScrollBar()
             scrollx.setValue(scrollx.value() + delta)
