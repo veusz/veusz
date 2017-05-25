bbd64cb7a4fb79fe2b5b36aab2dc783034505361
Jeremy Sanders <jeremy@jeremysanders.net>
2017-05-25 10:59:51 +0200

    Minor work

--- veusz/dataimport/dialog_fits2.py
+++ veusz/dataimport/dialog_fits2.py
@@ -16,6 +16,7 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ##############################################################################
 
+
 from __future__ import division, print_function
 
 from .. import qtall as qt4
@@ -26,19 +27,105 @@ from ..compat import cstr
 from . import base
 from . import defn_hdf5
 
-from .importtree import Node, ErrorNode, GenericTreeModel, ImportNameDeligate
+from . import fits_hdf5_tree
+from . import fits_hdf5_helpers
 
-def _(text, disambiguation=None, context="Import_HDF5"):
+def _(text, disambiguation=None, context="Import_FITS"):
     return qt4.QCoreApplication.translate(context, text, disambiguation)
 
 # lazily imported
 fits = None
 
-# name for the columns
-_ColName = 0
-_ColDataType = 1
-_ColShape = 2
-_ColToImport = 3
-_ColImportName = 4
-_ColSlice = 5
+def loadFitsModule():
+    global fits
+    try:
+        from astropy.io import fits
+    except ImportError:
+        try:
+            import pyfits as fits
+        except ImportError:
+            pass
+
+def constructTree(fitsfile):
+    """Turn fits file into a tree of nodes.
+
+    Returns root and list of nodes showing datasets
+    """
+
+    datanodes = []
+
+    def computedatatype(dtype):
+        datatype = 'invalid'
+        k = dtype.kind
+        if k in ('b', 'i', 'u', 'f'):
+            datatype = 'numeric'
+        elif k in ('S', 'a'):
+            datatype = 'text'
+        return datatype
+
+    def makedatanode(parent, ds):
+        # combine shape from dataset and column (if any)
+        shape = tuple(list(ds.shape)+list(ds.dtype.shape))
+        dtype = computedatatype(ds.dtype)
+
+        vszattrs = {}
+        for attr in ds.attrs:
+            if attr[:4] == 'vsz_':
+                vszattrs[attr] = defn_hdf5.bconv(ds.attrs[attr])
+
+        return fits_hdf5_tree.FileDataNode(
+            parent, ds.name, vszattrs, dtype, ds.dtype, shape)
+
+    def addsub(parent, grp):
+        """To recursively iterate over each parent."""
+        for child in sorted(grp.keys()):
+            try:
+                hchild = grp[child]
+            except KeyError:
+                continue
+            if isinstance(hchild, h5py.Group):
+                childnode = fits_hdf5_tree.FileGroupNode(parent, hchild)
+                addsub(childnode, hchild)
+            elif isinstance(hchild, h5py.Dataset):
+                try:
+                    dtype = hchild.dtype
+                except TypeError:
+                    # raised if datatype not supported by h5py
+                    continue
+
+                if dtype.kind == 'V':
+                    # compound data type - add a special group for
+                    # the compound, then its children
+                    childnode = fits_hdf5_tree.FileCompoundNode(parent, hchild)
+
+                    for field in sorted(hchild.dtype.fields.keys()):
+                        # get types and shape for individual sub-parts
+                        fdtype = hchild.dtype[field]
+                        fdatatype = computedatatype(fdtype)
+                        fshape = tuple(
+                            list(hchild[field].shape)+list(fdtype.shape))
+
+                        fattrs = fits_hdf5_helpers.filterAttrsByName(
+                            hchild.attrs, field)
+                        fnode = fits_hdf5_tree.FileDataNode(
+                            childnode,
+                            hchild.name+'/'+field,
+                            fattrs,
+                            fdatatype,
+                            fdtype,
+                            fshape)
+
+                        childnode.children.append(fnode)
+                        datanodes.append(fnode)
+
+                else:
+                    # normal dataset
+                    childnode = makedatanode(parent, hchild)
+                    datanodes.append(childnode)
+
+            parent.children.append(childnode)
+
+    root = fits_hdf5_tree.FileGroupNode(None, hdf5file)
+    addsub(root, hdf5file)
+    return root, datanodes
 
