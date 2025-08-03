veusz/veusz

commit 357fe0e571513a0328c1d37fd075ec1aec3a4b1d

bugfix: Overlapping of FormatDock tabs on the titlebar (#749)

veusz/windows/treeeditwindow.py
@@ -701,6 +701,12 @@ class FormatDock(qt.QDockWidget):
         self.document = document
         self.tabwidget = None
 
+        self.container = qt.QWidget()
+        self.layout = qt.QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.container.setLayout(self.layout)
+        self.setWidget(self.container)
+
         # update our view when the tree edit window selection changes
         treeedit.widgetsSelected.connect(self.selectedWidgets)
 
@@ -719,7 +725,7 @@ class FormatDock(qt.QDockWidget):
             self.tabwidget = None
 
         self.tabwidget = TabbedFormatting(self.document, setnsproxy)
-        self.setWidget(self.tabwidget)
+        self.layout.addWidget(self.tabwidget)
 
         # wrap tab from zero to max number
         tab = max( min(self.tabwidget.count()-1, tab), 0 )

commit f2c926128c133a75d66f4b3fa3364315294af3c1

Try to work around a redraw issue caused by (#749 and #751)

veusz/windows/treeeditwindow.py
@@ -703,12 +703,6 @@ class FormatDock(qt.QDockWidget):
         self.document = document
         self.tabwidget = None
 
-        self.container = qt.QWidget()
-        self.layout = qt.QVBoxLayout()
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.container.setLayout(self.layout)
-        self.setWidget(self.container)
-
         # update our view when the tree edit window selection changes
         treeedit.widgetsSelected.connect(self.selectedWidgets)
 
@@ -722,16 +716,21 @@ class FormatDock(qt.QDockWidget):
             tab = 0
 
         # delete old tabwidget
-        oldItem = self.layout.takeAt(0)
-        if oldItem:
-            oldWidget = oldItem.widget()
-            if oldWidget:
-                oldWidget.setParent(None)
-                oldWidget.deleteLater()
-                self.tabwidget = None
+        if self.tabwidget:
+            self.tabwidget.deleteLater()
+            self.tabwidget = None
 
         self.tabwidget = TabbedFormatting(self.document, setnsproxy)
-        self.layout.addWidget(self.tabwidget)
+
+        # this is a workaround for the tabs overlapping the title of
+        # the dock on MacOS (#749)
+        container = qt.QWidget()
+        layout = qt.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.tabwidget)
+        container.setLayout(layout)
+
+        self.setWidget(container)
 
         # wrap tab from zero to max number
         tab = max( min(self.tabwidget.count()-1, tab), 0 )